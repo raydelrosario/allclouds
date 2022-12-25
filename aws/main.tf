be3f782cc9cf7fbfa1276429336295aa4bf3db78
Ray Del Rosario <51274850+raydelrosario@users.noreply.github.com>
2022-12-25 09:12:32 -0500

    Add aws

---
+++ aws/main.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "secondary" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_iam_role" "main" {
+  name               = "allclouds-eks-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_eks_cluster" "main" {
+  name     = "allclouds-cluster"
+  role_arn = aws_iam_role.main.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.main.id, aws_subnet.secondary.id]
+  }
+}
